46b835720df030044837568dedb2bfba0e542d51
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-09-12 20:19:15 +0530

    Express Router + Cookies

--- routes/review.js
+++ routes/review.js
@@ -1,9 +1,10 @@
 const express = require("express")
-const router = express.Router()
+const router = express.Router({ mergeParams: true })
 const wrapAsync = require("../utils/wrapAsync.js")
 const ExpressError = require("../utils/ExpressError")
 const { reviewSchema } = require("../schema.js")
 const Review = require("../models/review.js")
+const Listing = require("../models/listing.js")
 
 
 const validateReview = (req, res, next) => {
