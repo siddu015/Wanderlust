f988fc4eae4d91f93e0e90c2d4f9796334415216
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-10-28 20:28:53 +0530

    Implemented session cookies, added flash messages for success and error states, and finalized signup and login forms

--- app.js
+++ app.js
@@ -88,7 +88,6 @@ app.all("*", (req, res, next) => {
 app.use((err, req, res, next) => {
     let {statusCode = 500, message = "Something went wrong!"} = err
     res.status(statusCode).render("error.ejs", { message })
-    // res.status(statusCode).send(message)
 })
 
 //Patch Route
