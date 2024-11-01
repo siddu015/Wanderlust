d5da8356a3c673b012779db157c594acd50a62c9
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-11-02 01:53:28 +0530

    Connected the database to mongodb Atlas for production

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require('method-override');
 const ejsMate = require("ejs-mate")
 const ExpressError = require("./utils/ExpressError")
 const session = require("express-session")
+const MongoStore = require('connect-mongo')
 const flash = require("connect-flash")
 const passport = require("passport")
 const LocalStrategy = require("passport-local")
@@ -20,31 +21,8 @@ const reviewsRouter = require("./routes/review.js")
 const userRouter = require("./routes/user.js")
 
 const port = 8080
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
+let dbUrl = process.env.ATLASDB_URL
 
-app.use((req, res, next) => {
-    req.responseTime = new Date(Date.now()).toString(); // Attach response time to req object
-    console.log(req.method, req.path, req.responseTime, req.hostname); // Log request details
-    next(); // Passes control to the next middleware
-});
-
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(methodOverride('_method'));
-app.engine('ejs', ejsMate)
-app.use(express.static(path.join(__dirname, "/public")))
-
-const sessionOptions = {
-    secret: "TheKeyIsHiddenInLock",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-    },
-}
 
 main()
     .then(() => {
@@ -55,7 +33,39 @@ main()
     })
 
 async function main(){
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate)
+app.use(express.static(path.join(__dirname, "/public")))
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600
+})
+
+store.on("error", () => {
+    console.log("Error in MONGO SESSION:", err)
+})
+
+
+const sessionOptions = {
+    store,
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
 }
 
 //Home Route
@@ -72,6 +82,12 @@ passport.use(new LocalStrategy(User.authenticate()))
 
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
+
+app.use((req, res, next) => {
+    req.responseTime = new Date(Date.now()).toString();
+    console.log(req.method, req.path, req.responseTime, req.hostname);
+    next();
+});
 
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
